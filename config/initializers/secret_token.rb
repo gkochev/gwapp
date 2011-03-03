e55488b785d7d28575149af0a0700383011f304e
george kochev <gkochev@gwu.edu>
2011-03-03 15:28:25 -0500

    Initial coommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gwapp::Application.config.secret_token = 'bdc01e81cccd458dc73fc7f86001319e9c1f05518c821bbb45051eeb72e0734dcdd32e27085571cecb6098a203db1c98fdabc816fe136b4b43b895bfbf4a3a37'
